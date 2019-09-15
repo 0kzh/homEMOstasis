95e40d97672dcb74ebec44a9078646222dd6e7b2
Caroline Liu <caroline.liu111@gmail.com>
2019-09-15 00:26:10 -0400

    fixed db

--- emotion_api_demo/get_response.py
+++ emotion_api_demo/get_response.py
@@ -4,6 +4,7 @@ import requests
 from os import path
 from emotion_api_demo import voiceSignal
 import azure.cosmos.cosmos_client as cosmos_client
+import azure.cosmos.documents as documents
 
 class Response(Thread):
     def __init__(self, emotion):
@@ -28,7 +29,29 @@ class Response(Thread):
             database_link = 'dbs/EmotionResponses'
 
             database = client.ReadDatabase(database_link)
-            print('Database with id \'{0}\' was found, it\'s _self is {1}'.format(id, database['_self']))
+            print('Database with id \'{0}\' was found, it\'s _self is {1}'.format('EmotionResponses', database['_self']))
+
+            try:
+                query = {'query': 'SELECT c.messages FROM c WHERE c.emotion = "anger"'}
+                options = {}
+                options['enableCrossPartitionQuery'] = True
+                message = "Document(s) found by query: "
+                results = list(client.QueryItems('dbs/EmotionResponses/colls/Items', query, options))
+                print(message)
+                for doc in results:
+                    print(doc)
+                return results
+            except errors.HTTPFailure as e:
+                if e.status_code == 404:
+                    print("Document doesn't exist")
+                elif e.status_code == 400:
+                    # Can occur when we are trying to query on excluded paths
+                    print("Bad Request exception occured: ", e)
+                    pass
+                else:
+                    raise
+            finally:
+                print()
 
         except errors.HTTPFailure as e:
             if e.status_code == 404:
@@ -37,12 +60,33 @@ class Response(Thread):
                 raise
 
         self.emotion = emotion
+        self.print("hi")
+        self.print(emotion)
         self.key = ""
 
         # For file debug
         script_dir = path.dirname(__file__) #<-- absolute dir the script is in
         rel_path = "../logs/debug.log"
         abs_file_path = path.join(script_dir, rel_path)
+
+        # try:
+        #     results = list(client.QueryItems(database_link, {'query': 'SELECT c.messages FROM c WHERE c.emotion = self.emotion'}))
+        #     print(message)
+        #     print('success')
+        #     for doc in results:
+        #         print(doc)
+        #     return results
+        # except errors.HTTPFailure as e:
+        #     if e.status_code == 404:
+        #         print("Document doesn't exist")
+        #     elif e.status_code == 400:
+        #         # Can occur when we are trying to query on excluded paths
+        #         print("Bad Request exception occured: ", e)
+        #         pass
+        #     else:
+        #         raise
+        # finally:
+        #     print()
 
         self.fout = open(abs_file_path, "a")
 
@@ -51,8 +95,8 @@ class Response(Thread):
         voice.get_token()
         voice.save_audio()
     
-    def __del__(self):
-        self.fout.close()
+    # def __del__(self):
+    #     self.fout.close()
 
     def fetchData(self):
         # Add query code here.
