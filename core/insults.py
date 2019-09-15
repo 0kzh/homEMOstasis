2c775e04c8595ff9bb8d156f3fe9f5674836dab0
Raymond Li <raymond@Raymonds-MacBook-Pro.local>
2019-09-15 08:31:47 -0400

    added query file

--- core/insults.py
+++ core/insults.py
@@ -9,12 +9,9 @@ class InsultGenerator:
     def __init__(self):
         pass
 
-    def gen_insult(self):
-        return self.insult()
-
-    def insult(self):
+    def insult(self, emotion):
         #r = requests.get('https://evilinsult.com/generate_insult.php?lang=en&type=json')
-        r = fetchData(e.get_emotion)
+        r = fetchData(emotion)
         list_res = r[0].get('messages')
         size = len(list_res)
         ran_num = random.randint(0, size-1)
