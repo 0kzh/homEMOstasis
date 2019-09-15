520dc693e724ee5714d823dd3b8526dc84b1b15c
NCNostalgia <jeff.ma@edu.uwaterloo.ca>
2019-09-14 20:14:41 -0400

    Change code

--- emotion_api_demo/voiceSignal.py
+++ emotion_api_demo/voiceSignal.py
@@ -4,9 +4,10 @@ import time
 from xml.etree import ElementTree
 
 class TextToSpeech(object):
-    def __init__(self, subscription_key):
+    def __init__(self, message, subscription_key):
         self.subscription_key = subscription_key
-        self.tts = input("What would you like to convert to speech: ")
+        # self.tts = input("What would you like to convert to speech: ")
+        self.tts = message
         self.timestr = time.strftime("%Y%m%d-%H%M")
         self.access_token = None
 
@@ -49,6 +50,6 @@ class TextToSpeech(object):
 
 if __name__ == "__main__":
     subscription_key = 'c989d0f018194adca6f46bcd25547ca7'
-    app = TextToSpeech(subscription_key)
+    app = TextToSpeech("Hello world"<subscription_key)
     app.get_token()
     app.save_audio()
